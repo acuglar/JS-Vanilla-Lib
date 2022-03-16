8398f1a7e037f499752b4f0c890f774dd0e505be
Alexandre Uglar <acuglar@gmail.com>
2022-03-16 04:24:51 -0300

    Add event listeners

---
+++ DOM/Carousel/app.js
@@ -0,0 +1,28 @@
+const slides = document.querySelectorAll('[data-js="carousel__item"]')
+const nextButton = document.querySelector('[data-js="carousel__button--next"]')
+const previousButton = document.querySelector('[data-js="carousel__button--prev"]')
+
+const lastSlideIndex = slides.length - 1
+let currentSlideIndex = 0
+
+const manipulateSlidesClasses = correctSlideIndex => {
+  slides.forEach(slide => slide.classList.remove('carousel__item--visible'))
+  slides[correctSlideIndex].classList.add('carousel__item--visible')
+}
+
+nextButton.addEventListener('click', () => {
+  const correctSlideIndex = currentSlideIndex === lastSlideIndex
+    ? currentSlideIndex = 0
+    : ++currentSlideIndex
+
+  manipulateSlidesClasses(correctSlideIndex)
+})
+
+previousButton.addEventListener('click', () => {
+  const correctSlideIndex = currentSlideIndex === 0
+    ? currentSlideIndex = lastSlideIndex
+    : --currentSlideIndex
+
+  manipulateSlidesClasses(correctSlideIndex)
+
+})
