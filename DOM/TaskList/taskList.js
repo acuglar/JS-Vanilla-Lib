f7eb7b2e123eedf012abeafc21b30a5e08a7778b
Alexandre Uglar <acuglar@gmail.com>
2022-03-18 12:22:38 -0300

    Create list of tasks

--- DOM/TaskList/taskList.js
+++ DOM/TaskList/taskList.js
@@ -2,16 +2,31 @@ const campoTextoTarefa = document.getElementById('campo-texto-tarefa')
 const botaoSubmitTarefa = document.getElementById('botao-submit-tarefa')
 const listaTarefas = document.getElementById("lista-tarefas")
 
+let colecaoTarefas = []
+
 const criarTarefa = () => {
   let tarefa = pegarTarefa()
+
   if (tarefa !== "" && tarefa !== " ") {
-    adicionarNovaTarefa(tarefa)
+    colecaoTarefas.push(tarefa);
+    listarTarefas(colecaoTarefas);
   } else {
     alertarUsuario("Preencha o campo corretamente!")
   }
 }
 
+const listarTarefas = (todasTarefas) => {
+  console.log(todasTarefas);
+
+  limparLista();
+  for (let contador = 0; contador < todasTarefas.length; contador += 1) {
+    adicionarNovaTarefa(todasTarefas[contador]);
+  }
+}
+
 const limparCampoTexto = () => campoTextoTarefa.value = ''
+
+const limparLista = () => listaTarefas.innerHTML = ''
 
 const pegarTarefa = () => {
   const tarefa = campoTextoTarefa.value
