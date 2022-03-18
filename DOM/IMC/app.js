1759648f725dd131c9ae971794e9743c719f606f
Alexandre Uglar <acuglar@gmail.com>
2022-03-18 04:28:05 -0300

    Calculate bmi

--- DOM/IMC/app.js
+++ DOM/IMC/app.js
@@ -2,6 +2,7 @@ const weightSlider = document.getElementById("sliderWeight");
 const weightOutput = document.getElementById("weightOutput");
 const heightSlider = document.getElementById("sliderHeight");
 const heightOutput = document.getElementById("heightOutput");
+const button = document.getElementById("btnCalcular");
 
 function updateInputValue(e) {
   console.log(e.target.id);
@@ -23,3 +24,15 @@ weightOutput.addEventListener("input", updateInputValue);
 weightSlider.addEventListener("input", updateInputValue);
 heightOutput.addEventListener("input", updateInputValue);
 heightSlider.addEventListener("input", updateInputValue);
+
+const bmiCalculator = (weight, height) => {
+  const heightMeters = height / 100
+  return (weight / (heightMeters * heightMeters)).toFixed(2)
+}
+
+const showBmi = (bmi) => {
+  const output = document.querySelector("#resultado p")
+  output.textContent = bmiCalculator(weightOutput.value, heightOutput.value)
+}
+
+button.addEventListener("click", showBmi);
